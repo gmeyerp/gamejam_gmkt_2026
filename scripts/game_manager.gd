6095df0e51c89b8d2524a278f71626bed76374b1
Gabriel Meyer <159436065+gmeyerp@users.noreply.github.com>
2026-07-22 19:44:43 -0300

    feat game manager inicial

---
+++ scripts/game_manager.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var is_playing: bool
+@export var max_game_time: float = 15
+var game_time: float = 0
+
+func game_start():
+	game_time = 0
+	is_playing = true
+
+func _process(delta: float) -> void:
+	if is_playing:
+		game_time += delta
+		print(game_time)
+
+func _on_start_button_pressed() -> void:
+	game_start()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
