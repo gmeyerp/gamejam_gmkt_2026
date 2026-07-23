32ad8f0e76378ca7c17492c0862ba8c7762a6cb6
Arthur <86436882+arthurmordente@users.noreply.github.com>
2026-07-22 22:08:48 -0300

    Reapply "Merge branch 'main' into arthur_teste"
    This reverts commit 0b036557e5bb5bf919200ecca17e4cc53efbb88e.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,20 +1,36 @@
 extends Node
+class_name GameManager
 
-var is_playing: bool
-@export var max_game_time: float = 15
-var game_time: float = 0
+@export var demission_manager: DemissionManager
+@export var post_processing: CanvasLayer
 
-func game_start():
-	game_time = 0
+var is_playing: bool = false
+@export var max_game_time: float = 15.0
+var game_time: float = 0.0
+
+func _ready() -> void:
+	if demission_manager:
+		demission_manager.game_finished.connect(_on_demission_game_finished)
+
+func game_start() -> void:
+	game_time = 0.0
 	is_playing = true
+	post_processing.set_distortion(0)
+	
+	if demission_manager:
+		demission_manager.start_game()
 
 func _process(delta: float) -> void:
 	if is_playing:
 		game_time += delta
+		post_processing.set_distortion(clamp(game_time/max_game_time, 0, 1 ))
 
 func _on_start_button_pressed() -> void:
 	game_start()
 
-
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_demission_game_finished() -> void:
+	is_playing = false
+	print("Acabou os funcionários! Tempo total: ", game_time)
