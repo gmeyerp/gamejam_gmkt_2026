18d5dbc021608323db653d9ec3e3a0153ef33d6d
Luiz Gustavo <luizgcostaf@gmail.com>
2026-07-24 07:16:03 -0300

    Fix: Rulebook UI department statistics
    Corrigido a obtenção das médias dos departamentos no computador.

--- scripts/desk/computer.gd
+++ scripts/desk/computer.gd
@@ -1,33 +1,40 @@
-class_name DeskComputer
 extends Node3D
+class_name DeskComputer
 
 @export var rulebook_ui: ComputerRulebookUI
 @export var diegetic_display: DiegeticUIDisplay
 @export var interaction_router: InteractionRouter
 
-
 func _ready() -> void:
 	if rulebook_ui == null and diegetic_display:
 		rulebook_ui = diegetic_display.get_ui() as ComputerRulebookUI
-	if rulebook_ui:
-		rulebook_ui.set_department_data(get_department_averages(GlobalVariables.Department.Office))
-		rulebook_ui.show_ui()
+		
+	_update_computer_ui()
+	
 	if diegetic_display:
 		diegetic_display.set_input_active(false)
 
-
 func on_interact() -> void:
-	if rulebook_ui:
-		rulebook_ui.set_department_data(get_department_averages(GlobalVariables.Department.Office))
-		rulebook_ui.show_ui()
+	_update_computer_ui()
 	if diegetic_display:
 		diegetic_display.set_input_active(true)
-
 
 func on_deselect() -> void:
 	if diegetic_display:
 		diegetic_display.set_input_active(false)
 
+func _update_computer_ui() -> void:
+	if rulebook_ui:
+		var current_dept = GlobalVariables.Department.Office
+		rulebook_ui.set_department_data(get_department_averages(current_dept))
+		rulebook_ui.show_ui()
 
-func get_department_averages(_dept: GlobalVariables.Department) -> Dictionary:
-	return {}
+func get_department_averages(dept: GlobalVariables.Department) -> Dictionary:
+	var avg_salary = EmployeeList.get_average_salary(dept)
+	var avg_productivity = EmployeeList.get_average_productivity(dept)
+	
+	return {
+		"department": dept,
+		"average_salary": avg_salary,
+		"average_productivity": avg_productivity
+	}
--- scripts/gameplay_controller.gd
+++ scripts/gameplay_controller.gd
@@ -6,29 +6,28 @@ extends Node
 @export var interaction_router: InteractionRouter
 @export var pc_focus_marker: Marker3D
 
-
 func _ready() -> void:
 	_enter_menu()
-
 
 func on_start_game_pressed() -> void:
 	_begin_play()
 
-
 func _enter_menu() -> void:
 	if interaction_router:
 		interaction_router.set_active(false)
+		
 	if camera and pc_focus_marker:
 		camera.edge_look_enabled = false
 		camera.snap_focus(pc_focus_marker)
-
 
 func _begin_play() -> void:
 	if title_ui:
 		title_ui.visible = false
 		title_ui.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		
 	if camera:
 		camera.edge_look_enabled = true
 		camera.set_mode(CameraController.Mode.FREE)
+		
 	if interaction_router:
 		interaction_router.set_active(true)
