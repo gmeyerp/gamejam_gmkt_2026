4a683fb87a55fd804b7ac18bbea1ecd5fa4d8a57
Luiz Gustavo <146591414+luudsk@users.noreply.github.com>
2026-07-24 07:49:56 -0300

    Fix: Office cleanup and light repair issues
    Corrigidos os bugs que impediam a limpeza correta do escritório e podiam causar falhas durante a execução.

--- scripts/environment_manager.gd
+++ scripts/environment_manager.gd
@@ -1,23 +1,28 @@
 extends Node3D
 class_name EnvironmentManager
-var max_cleaners : int
-var max_maintenance : int
 
-@export var lights : Array[Light3D]
+var max_cleaners: int
+var max_maintenance: int
+
+@export var lights: Array[Light3D]
 @onready var game_lights = lights.duplicate()
-@export var dirt : Array [Node3D]
+@export var dirt: Array[Node3D]
 @export var garbage_speed: float = 0
 @export var garbage: PackedScene
+
 var garbage_timer: float = 0
 const DUMP_TIMER: float = 10
 
 func clear_office():
-	for i in range(lights.size()):
-		game_lights[i].repair()
-		game_lights = lights.duplicate()
-	for i in range(dirt.size(), -1):
-		for child in dirt[i].get_chidren():
+	for light in lights:
+		if light.has_method("repair"):
+			light.repair()
+	game_lights = lights.duplicate()
+	
+	for node in dirt:
+		for child in node.get_children():
 			child.queue_free()
+			
 	garbage_speed = 0
 	garbage_timer = 0
 
@@ -33,7 +38,8 @@ func on_maintenance_fired():
 	if game_lights.size() == 0:
 		return
 	var rand = randi_range(0, game_lights.size() - 1)
-	game_lights[rand].break_light()
+	if game_lights[rand].has_method("break_light"):
+		game_lights[rand].break_light()
 	game_lights.remove_at(rand)
 
 func on_cleaner_fired():
@@ -42,12 +48,12 @@ func on_cleaner_fired():
 
 func _process(delta: float) -> void:
 	garbage_timer += garbage_speed * delta
-	if (garbage_timer >= DUMP_TIMER):
+	if garbage_timer >= DUMP_TIMER:
 		garbage_timer = 0
 		create_garbage()
 
 func create_garbage():
-	if dirt.size() == 0:
+	if dirt.size() == 0 or not garbage:
 		return
 	var instance = garbage.instantiate()
 	var rand = randi_range(0, dirt.size() - 1)
